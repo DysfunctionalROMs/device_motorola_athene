2f4b44adbdcf39ec7bfde0d1de3ef37ecfb704b1
TeamFahQ <teamfahq1@gmail.com>
2017-01-06 22:56:41 -0600

    Add LZ kernel & fix dependencies typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,10 +49,12 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-#TARGET_CUSTOM_DTBTOOL := dtbTool_moto
+TARGET_CUSTOM_DTBTOOL := dtbTool_moto
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := cm_athene_defconfig
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8952
+TARGET_KERNEL_CONFIG := athene-lz_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/msm8952-lz
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-cortex_a15-linux-gnueabihf-
 
 # Audio
 AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
